bc68155c8b97095dfe77fc55e38d7fc1486b305d
Killian Audic <killianaudic@aol.fr>
2021-10-29 18:01:41 +0200

    début de la mise en place du fichier sudoku.js

--- js/app.js
+++ js/app.js
@@ -89,6 +89,7 @@ const app = {
 
         const emptyBoardBtnElt = document.createElement('button');
         emptyBoardBtnElt.textContent = 'Vider la grille';
+        emptyBoardBtnElt.type = 'button';
         emptyBoardBtnElt.addEventListener('click', app.emptyExercice);
 
         const solveExerciceBtnElt = document.createElement('button');
@@ -102,6 +103,261 @@ const app = {
         formElt.appendChild(loadExerciceBtnElt);
         formElt.appendChild(emptyBoardBtnElt);
         formElt.appendChild(solveExerciceBtnElt);
+
+    },
+
+    emptyExercice: () => {
+
+        //Mettre la grille à 0.
+        console.log('Emptying Board ...');
+        const boardCells = document.querySelectorAll('.sudokValues');
+        for (let cell of boardCells) {
+            cell.value = '';
+        }
+
+    },
+
+    loadExercice: (e) => {
+
+        e.preventDefault();
+
+        //On met la grille à 0 au cas ou
+        app.emptyExercice();
+
+        //on regarde la sélection de l'exo faite par l'utilisateur pour savoir quoi charger
+        console.log('Loading Board ...');
+        const exerciceSelectorElt = document.getElementById('level-selection');
+        const optionExerciceSelected = exerciceSelectorElt.selectedIndex;
+        const ExerciceChosen = exerciceSelectorElt.options[optionExerciceSelected].value;
+
+        if (ExerciceChosen == 'Facile1') {
+
+            //précharge une grille de closer du 04 au 10 Juin 2021
+            document.getElementById('0-0').value = '';
+            document.getElementById('0-1').value = '';
+            document.getElementById('0-2').value = 4;
+            document.getElementById('0-3').value = 6;
+            document.getElementById('0-4').value = '';
+            document.getElementById('0-5').value = '';
+            document.getElementById('0-6').value = '';
+            document.getElementById('0-7').value = 8;
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('1-0').value = 3;
+            document.getElementById('0-1').value = '';
+            document.getElementById('1-2').value = 5;
+            document.getElementById('0-3').value = '';
+            document.getElementById('1-4').value = 1;
+            document.getElementById('0-5').value = '';
+            document.getElementById('0-6').value = '';
+            document.getElementById('1-7').value = 6;
+            document.getElementById('1-8').value = 7;
+
+            document.getElementById('2-0').value = 6;
+            document.getElementById('0-1').value = '';
+            document.getElementById('0-2').value = '';
+            document.getElementById('0-3').value = '';
+            document.getElementById('2-4').value = 5;
+            document.getElementById('2-5').value = 3;
+            document.getElementById('2-6').value = 9;
+            document.getElementById('0-7').value = '';
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('3-0').value = 9;
+            document.getElementById('0-1').value = '';
+            document.getElementById('0-2').value = '';
+            document.getElementById('0-3').value = '';
+            document.getElementById('3-4').value = 8;
+            document.getElementById('0-5').value = '';
+            document.getElementById('0-6').value = '';
+            document.getElementById('0-7').value = '';
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('0-0').value = '';
+            document.getElementById('4-1').value = 1;
+            document.getElementById('0-2').value = '';
+            document.getElementById('0-3').value = '';
+            document.getElementById('4-4').value = 2;
+            document.getElementById('4-5').value = 6;
+            document.getElementById('4-6').value = 4;
+            document.getElementById('0-7').value = '';
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('5-0').value = 4;
+            document.getElementById('0-1').value = '';
+            document.getElementById('0-2').value = '';
+            document.getElementById('0-3').value = '';
+            document.getElementById('0-4').value = '';
+            document.getElementById('0-5').value = '';
+            document.getElementById('5-6').value = 5;
+            document.getElementById('5-7').value = 1;
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('0-0').value = '';
+            document.getElementById('0-1').value = '';
+            document.getElementById('6-2').value = 6;
+            document.getElementById('0-3').value = '';
+            document.getElementById('0-4').value = '';
+            document.getElementById('6-5').value = 1;
+            document.getElementById('0-6').value = '';
+            document.getElementById('0-7').value = '';
+            document.getElementById('0-8').value = '';
+
+            document.getElementById('0-0').value = '';
+            document.getElementById('0-1').value = '';
+            document.getElementById('7-2').value = 1;
+            document.getElementById('7-3').value = 7;
+            document.getElementById('7-4').value = 9;
+            document.getElementById('0-5').value = '';
+            document.getElementById('0-6').value = '';
+            document.getElementById('0-7').value = '';
+            document.getElementById('7-8').value = 2;
+
+            document.getElementById('0-0').value = '';
+            document.getElementById('8-1').value = 3;
+            document.getElementById('0-2').value = '';
+            document.getElementById('0-3').value = '';
+            document.getElementById('8-4').value = 6;
+            document.getElementById('8-5').value = 2;
+            document.getElementById('0-6').value = '';
+            document.getElementById('8-7').value = 7;
+            document.getElementById('0-8').value = '';
+        }
+
+        else if (ExerciceChosen == 'Facile2') {
+            //précharge une grille de closer du 04 au 10 Juin 2021
+            document.getElementById('0-0').value = 6;
+            document.getElementById('0-2').value = 9;
+            document.getElementById('0-5').value = 5;
+            document.getElementById('0-7').value = 1;
+            document.getElementById('1-2').value = 4;
+            document.getElementById('1-3').value = 3;
+            document.getElementById('1-4').value = 7;
+            document.getElementById('1-5').value = 6;
+            document.getElementById('1-8').value = 2;
+            document.getElementById('2-0').value = 8;
+            document.getElementById('2-1').value = 7;
+            document.getElementById('2-4').value = 1;
+            document.getElementById('2-7').value = 6;
+            document.getElementById('3-0').value = 7;
+            document.getElementById('3-5').value = 9;
+            document.getElementById('3-7').value = 3;
+            document.getElementById('3-8').value = 1;
+            document.getElementById('4-1').value = 9;
+            document.getElementById('5-0').value = 3;
+            document.getElementById('5-2').value = 1;
+            document.getElementById('5-3').value = 5;
+            document.getElementById('5-5').value = 2;
+            document.getElementById('6-0').value = 2;
+            document.getElementById('6-2').value = 3;
+            document.getElementById('6-6').value = 6;
+            document.getElementById('6-7').value = 7;
+            document.getElementById('7-1').value = 6;
+            document.getElementById('7-4').value = 9;
+            document.getElementById('7-5').value = 3;
+            document.getElementById('7-6').value = 1;
+            document.getElementById('7-8').value = 8;
+            document.getElementById('8-0').value = 9;
+            document.getElementById('8-5').value = 1;
+
+        }
+
+        else if (ExerciceChosen == 'Moyen') {
+            //précharge une grille de closer du 04 au 10 Juin 2021
+            document.getElementById('0-0').value = 4;
+            document.getElementById('0-5').value = 6;
+            document.getElementById('0-8').value = 7;
+            document.getElementById('1-3').value = 3;
+            document.getElementById('1-4').value = 8;
+            document.getElementById('1-7').value = 6;
+            document.getElementById('1-8').value = 1;
+            document.getElementById('2-1').value = 3;
+            document.getElementById('2-5').value = 4;
+            document.getElementById('3-1').value = 2;
+            document.getElementById('3-5').value = 9;
+            document.getElementById('3-7').value = 5;
+            document.getElementById('4-0').value = 3;
+            document.getElementById('4-1').value = 7;
+            document.getElementById('4-6').value = 2;
+            document.getElementById('5-0').value = 1;
+            document.getElementById('5-2').value = 5;
+            document.getElementById('5-6').value = 3;
+            document.getElementById('5-7').value = 4;
+            document.getElementById('6-3').value = 5;
+            document.getElementById('6-5').value = 8;
+            document.getElementById('6-7').value = 9;
+            document.getElementById('7-1').value = 1;
+            document.getElementById('7-4').value = 2;
+            document.getElementById('7-7').value = 8;
+            document.getElementById('7-8').value = 4;
+            document.getElementById('8-3').value = 6;
+
+        }
+
+        else if (ExerciceChosen == 'Difficile') {
+            //précharge une grille de closer du 04 au 10 Juin 2021
+            document.getElementById('0-1').value = 4;
+            document.getElementById('0-4').value = 5;
+            document.getElementById('0-7').value = 9;
+            document.getElementById('1-2').value = 2;
+            document.getElementById('1-6').value = 8;
+            document.getElementById('1-8').value = 5;
+            document.getElementById('2-1').value = 9;
+            document.getElementById('2-8').value = 1;
+            document.getElementById('3-0').value = 8;
+            document.getElementById('3-3').value = 1;
+            document.getElementById('3-6').value = 3;
+            document.getElementById('3-7').value = 4;
+            document.getElementById('4-1').value = 7;
+            document.getElementById('4-2').value = 4;
+            document.getElementById('4-5').value = 8;
+            document.getElementById('5-8').value = 9;
+            document.getElementById('6-0').value = 5;
+            document.getElementById('6-1').value = 1;
+            document.getElementById('6-3').value = 2;
+            document.getElementById('7-1').value = 6;
+            document.getElementById('7-3').value = 3;
+            document.getElementById('7-6').value = 9;
+            document.getElementById('8-4').value = 6;
+            document.getElementById('8-7').value = 7;
+
+        }
+
+        else if (ExerciceChosen == 'Démoniaque') {
+            //précharge une grille de closer du 04 au 10 Juin 2021
+            document.getElementById('0-2').value = 1;
+            document.getElementById('0-7').value = 2;
+            document.getElementById('1-3').value = 8;
+            document.getElementById('1-5').value = 5;
+            document.getElementById('1-8').value = 1;
+            document.getElementById('2-0').value = 5;
+            document.getElementById('2-3').value = 1;
+            document.getElementById('2-5').value = 3;
+            document.getElementById('2-6').value = 4;
+            document.getElementById('3-1').value = 2;
+            document.getElementById('3-2').value = 9;
+            document.getElementById('3-6').value = 7;
+            document.getElementById('3-7').value = 4;
+            document.getElementById('5-1').value = 4;
+            document.getElementById('5-2').value = 6;
+            document.getElementById('5-6').value = 3;
+            document.getElementById('5-7').value = 9;
+            document.getElementById('6-2').value = 2;
+            document.getElementById('6-3').value = 3;
+            document.getElementById('6-5').value = 7;
+            document.getElementById('6-8').value = 4;
+            document.getElementById('7-0').value = 9;
+            document.getElementById('7-3').value = 5;
+            document.getElementById('7-5').value = 1;
+            document.getElementById('8-1').value = 1;
+            document.getElementById('8-6').value = 8;
+
+        }
+
+
+
+
+
 
     },
 
@@ -124,19 +380,11 @@ const app = {
 
     },
 
-    loadExercice: () => {
-        //mettre le code de chargerExercice ici
-    },
-
-    emptyExercice: () => {
-        //mettre le code de  ici
-    },
-
     solveExercice: (e) => {
 
         e.preventDefault();
 
-        console.log('Bouton \"Résoudre Grille activé\"')
+        console.log('Bouton "Résoudre Grille activé"');
 
         //Déclencher le chronomètre
         app.launchStopWatchCount();
@@ -164,6 +412,7 @@ const app = {
 
         }, 1000);
     }
+
 
 };
 
--- js/sudoku.js
+++ js/sudoku.js
@@ -1,4 +1,94 @@
 const sudoku = {
 
+    boardData: null, //contiendra les données de la grille à chaque lecture
+
+    isBoardOk: false, //passera à true dès lors qu'il y aura une grille html et des données de saisies dedans
+
+    isBoardFinished: false, //passera à true si le solveur a réussi
+
+    nextCellToCalculate: null, //correspond à la prochaine cellule que le solveur doit calculer
+
+    lastCellResult: null, //correspond au calcul de la dernière cellule calculée
+
+    numeroResultUsed: 0, // lors du backtracing il correspond au numéro de résultat utilisé parmis les choix possibles
+
+    goBack: false, // lors du backtracing il corresponds au besoin ou non de faire un retour arrière
+
+    initialSolveBoard: () => {
+
+        sudoku.isBoardOk = sudoku.checkBoard(); //vérifie la présence d'une grille html & vérifie qu'il y a des chiffres de renseignés dans la grille
+
+        if (sudoku.isBoardOk) {
+            sudoku.boardData = sudoku.readBoard(); //on lit et on retourne un tableau 0: matrice vide, 1:Ligne, 2:colonne, 3:Carré
+
+            // TODO améliorer le système de 20 boucles en ayant un programme qui s'arrête de boucler automatiquement
+            for (let m = 0; m <= 20; m++) // on fait 20 boucles de calcul (chiffre qui peut être adapté)
+            {
+
+                for (let z = 0; z < sudoku.boardData[0].length; z++) {
+
+                    sudoku.nextCellToCalculate = sudoku.boardData[0][z];
+
+                    sudoku.lastCellResult = sudoku.calculateCell(sudoku.nextCellToCalculate, sudoku.boardData);
+
+                    if (sudoku.lastCellResult.length == 1) {
+                        const cell = document.getElementById(sudoku.nextCellToCalculate[0]);
+                        cell.value = sudoku.lastCellResult[0];
+
+                    }
+
+                }
+                sudoku.boardData = sudoku.readBoard();
+
+                sudoku.isBoardFinished = sudoku.calculateIfBoardFinished(sudoku.boardData);
+
+                if (sudoku.isBoardFinished) {
+                    alert('Grille Terminée!');
+                    return; //! ajout de ma part
+                }
+                else {
+                    // si après 20 boucles la grille est non résolue alors on lance la fonction backtracing 
+                    //avec le paramètre x=0 pour dire que l'on prendra le 1er résultat possible et qu'il ne s'agit pas d'un retour arrière
+
+                    //reject("Résolution simple échoue. Début Bactracing");
+                    console.log('Fin des 20 boucles: début du solveur bactracing.');
+                    sudoku.solveBoard(); //! j'ai créé une deuxième fonction par doute 
+
+                }
+
+            }
+        }
+
+
+    },
+
+    launchBackTracing: () => {
+
+    },
+
+    solveBoard: () => { //Résolution finale à contrario de la résolution initiale qui correspond aux 20 boucles
+
+    },
+
+    checkBoard: () => { //remplit sudoku.isBoardOk
+
+    },
+
+    readBoard: () => { //remplit boardData
+
+    },
+
+    calculateCell: () => {
+
+    },
+
+    calculateIfBoardFinished: () => {
+
+    },
+
+    loadExercice: () => {
+
+    }
+
 
 };
