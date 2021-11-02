5bc5e27d96b9f0a0285ca67469c842e974b6efb1
Killian Audic <killianaudic@aol.fr>
2021-11-02 07:44:03 +0100

    mise en place fichier json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-redeclare
 const app = {
 
     init: function () {
@@ -127,7 +128,18 @@ const app = {
         //test d'une fonction fetch
         fetch('http://localhost:3000/database')
             .then(function (response) {
-                console.log((response));
+                try {
+                    // Parse a JSON
+                    // const responseParsed = JSON.parse(response);
+                    console.log(response);
+                } catch (e) {
+                    // You can read e for more info
+                    // Let's assume the error is that we already have parsed the payload
+                    // So just return that
+                    const responseNotParsed = response;
+                    alert('erreur: ' + e + 'rep: ' + responseNotParsed);
+                }
+
             });
 
 
--- server/server.js
+++ server/server.js
@@ -1,39 +1,23 @@
-const boardDatabase =
-    [
-        [
-            [0, 0, 4, 6, 0, 0, 0, 8, 0], // Vraie grille mais les 0 sont normalement des ''
-            [3, 0, 5, 0, 1, 0, 0, 6, 7],
-            [6, 0, 0, 0, 5, 3, 9, 0, 0],
-            [9, 0, 0, 0, 8, 0, 0, 0, 0],
-            [0, 1, 0, 0, 2, 6, 4, 0, 0],
-            [4, 0, 0, 0, 0, 0, 5, 1, 0],
-            [0, 0, 6, 0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 7, 9, 0, 0, 0, 2],
-            [0, 3, 0, 0, 6, 2, 0, 7, 0],
-        ],
-        [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9], //Grille de test
-            [3, 0, 5, 0, 1, 0, 0, 6, 7],
-            [6, 0, 0, 0, 5, 3, 9, 0, 0],
-            [9, 0, 0, 0, 8, 0, 0, 0, 0],
-            [0, 1, 0, 0, 2, 6, 4, 0, 0],
-            [4, 0, 0, 0, 0, 0, 5, 1, 0],
-            [0, 0, 6, 0, 0, 1, 0, 0, 0],
-            [0, 0, 1, 7, 9, 0, 0, 0, 2],
-            [0, 3, 0, 0, 6, 2, 0, 7, 0],
-        ]
-    ];
 
-
+// eslint-disable-next-line no-undef
 const http = require('http');
+// eslint-disable-next-line no-undef
+const fs = require('fs');
+
 
 const server = http.createServer((req, res) => {
 
     if (req.url === '/database') {
 
-        res.setHeader('Content-Type', 'application/json');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.end(JSON.stringify(boardDatabase[1]));
+        fs.readFile('./boardDatabase.json', (err, data) => {
+            if (err) throw err;
+            console.log('boardDatabase: ' + data);
+            res.setHeader('Content-Type', 'application/json');
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.end(data);
+        });
+
+
     }
 
 });
