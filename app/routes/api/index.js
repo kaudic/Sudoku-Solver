937ed141c72ba62717697bdb702f27ad64ad4d8d
Killian Audic <killianaudic@aol.fr>
2022-06-06 09:17:59 +0200

    adding seo details

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -17,15 +17,15 @@ const settingsRouter = require('./settings');
 
 // Dispatch entre route d'accueil et routeurs secondaires
 router.all('/', apiController.home);
-router.use('/settings', settingsRouter);
-router.use('/db', dbRouter);
-router.use('/board', boardRouter);
-
 // page publique de consultation du fichier robot.txt
 router.get('/robots.txt', handler(seoController.renderRobotTxt));
 
 // page publique de consultation du fichier sitemap.xml
 router.get('/sitemap.xml', handler(seoController.renderSiteMapXml));
+router.use('/settings', settingsRouter);
+router.use('/db', dbRouter);
+router.use('/board', boardRouter);
+
 
 router.use(() => {
     throw new SudokError(404, 'API Route not found');
