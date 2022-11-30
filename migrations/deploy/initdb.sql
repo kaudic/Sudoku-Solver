6678430335312ef6b9f622216423b294dcb742fb
Killian Audic <killianaudic@aol.fr>
2022-11-30 09:41:25 +0100

    changed database.js

--- migrations/deploy/initdb.sql
+++ migrations/deploy/initdb.sql
@@ -46,7 +46,9 @@ VALUES
 --insertion du profil admin
 INSERT INTO "actor" ("actor_name", "actor_surname","actor_email","actor_login","actor_password","actor_role_id","actor_display_id")
 VALUES
-('Killian','Audic','killianaudic@gmail.com','kaudic','$2b$10$7uwE7IKBXN.oMT1qMgGag.KWSt4NH8vYBhRe6vo.dddWSw1B82Rtq','1','1');
+('Killian','Audic','killianaudic@gmail.com','kaudic','$2b$10$RrjOqifzG4OQzg0u58P0sONVuMuGaSiY/ZX/y4BVOcvUYpo4Imo5.','1','1');
 
+--insert one board to get started
+INSERT INTO board ("board_data") VALUES ('[[7,4,1,3,6,5,9,2,8],[9,2,6,7,4,8,3,5,1],[8,3,5,9,1,2,6,4,7],[1,6,9,4,2,3,8,7,5],[2,8,3,1,5,7,4,6,9],[5,7,4,6,8,9,1,3,2],[6,1,7,2,9,4,5,8,3],[3,9,8,5,7,6,2,1,4],[4,5,2,8,3,1,7,9,6]]');
 
 COMMIT;
