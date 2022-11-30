df5bd083fe2442aafd38e069f843847b2c772e76
Killian Audic <killianaudic@aol.fr>
2022-11-30 10:00:21 +0100

    changed in databaseWrite controller

--- app/controllers/api/dbController.js
+++ app/controllers/api/dbController.js
@@ -28,7 +28,7 @@ const controller = {
     async deleteBoard(req, res) {
         const id = Number(req.params.id);
         await dataMapper.deleteOneBoardById(id);
-        return res.redirect(`sudoku/sudoku-solver/database/`);
+        return res.redirect(`${process.env.BASE_URL}/sudoku/sudoku-solver/database/`);
     },
 };
 
